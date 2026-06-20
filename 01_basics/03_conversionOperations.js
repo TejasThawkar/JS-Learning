3e339d1bb4f087d047c381f442ae6412c1a61775
Tejas <ttthawkar17@gmail.com>
2026-06-21 01:29:02 +0530

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -19,3 +19,32 @@ let someNumber = 33;
 let StringNumber = String(someNumber);
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+
+// ******************Operations****************** //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+str1 = "Hello"
+str2 = "World"
+let str3 = str1 + " " + str2
+console.log(str3);
+
+
+console.log("1" + 2); //12
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3+4)*5%3)
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
