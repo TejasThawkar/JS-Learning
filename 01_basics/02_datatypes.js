a0d6d722c342ee0334d38c58a01642f4de8c247b
Tejas <ttthawkar17@gmail.com>
2026-06-21 00:53:25 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3 + 4); // we can use alert, we are using nodejs not browser, confirm in browser
+
+console.log(3 + 4); // we can use console.log
+
+let name = "Tejas"
+let age = 22
+let isApproved = true
+
+//number => 2 to power 53
+//bigint
+///string => '', "", ``
+//boolean => true, false
+//undefined => value not assigned
+//null => empty value
+//symbol => unique identifier
+
+
+//object => key value pair
+
+console.log(typeof "tejas");
+console.log(typeof null);
